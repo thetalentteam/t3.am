fb23da8ee35beac5a17319018d4500ee44200f06
Sai Perchard <sai@perchard.com>
2015-09-23 16:52:01 -0700

    Paging

---
+++ README.md
@@ -0,0 +1,30 @@
+This website uses [Middleman](https://middlemanapp.com/)
+
+## Setup
+
+Middleman is distributed using the RubyGems package manager. This means you will
+need both the Ruby language runtime installed and RubyGems to begin using
+Middleman.
+
+Mac OS X comes prepackaged with both Ruby and Rubygems, however, some of the
+Middleman's dependencies need to be compiled during installation and on OS X
+that requires Xcode. Xcode can be installed via the Mac App Store. Alternately,
+if you have a free Apple Developer account, you can just install Command Line
+Tools for Xcode from their downloads page.
+
+Once you have Ruby and RubyGems up and running, execute the following from the
+command line:
+
+    bundle install
+
+This will install all of the gems required for this project (including
+Middleman).
+
+## Run Local Server
+
+    middleman server
+
+## Build
+
+    middleman build
+
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,37 @@
 //= require_tree .
+
+var section_identifiers = ['hero', 'global', 'clients', 'aligned-incentives', 'technology-enabled', 'iaas', 'talent-engagement', 'studio', 'assessment-tools', 'testimonials', 'join'];
+
+$.each(section_identifiers, function(index, value) {
+
+  new Waypoint({
+    element: document.getElementById(value),
+    handler: function(direction) {
+      if (direction === 'down') {
+        $('.paging li').removeClass('active');
+        $('.paging li.'+value).addClass('active');
+        console.log('down triggered');
+      }
+    }
+  })
+
+  new Waypoint({
+    element: document.getElementById(value),
+    handler: function(direction) {
+      if (direction === 'up') {
+        $('.paging li').removeClass('active');
+        $('.paging li.'+value).addClass('active');
+        console.log('up triggered');
+      }
+    },
+    offset: '-99%'
+  })
+
+});
+
+$.each(section_identifiers, function(index, value) {
+  $('.paging li.'+value).on('click', function() {
+    $.scrollTo('#'+value, 300);
+  });
+});
+
